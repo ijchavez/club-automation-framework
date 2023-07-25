25708c39e414fbdab9cccbb507d57047817084e4
gravielsosa <sosagraviel@gmail.com>
2023-07-25 09:21:03 -0300

    category API

--- src/main/java/endpoints/category/CategoryTypeEndpoint.java
+++ src/main/java/endpoints/category/CategoryTypeEndpoint.java
@@ -12,6 +12,7 @@ import utils.constants.DataConstantQueries;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CategoryTypeEndpoint extends BaseEndpoint {
     protected RequestFactory requestFactory;
@@ -25,33 +26,64 @@ public class CategoryTypeEndpoint extends BaseEndpoint {
         return getResponseCategoryType(categoryPayload);
     }
 
+    /**
+     * this addCategory method prepare the category payload and name to pass through the
+     * getResponseCategoryType method by parameters
+     *
+     * @return
+     */
     public Response addCategory() {
         CategoryTypePayload categoryPayload = new CategoryTypePayload("Automation Category");
         return getResponseCategoryType(categoryPayload);
     }
 
+    /**
+     * this getResponseCategoryType send a post request to create a category
+     *
+     * @param categoryPayload it's the body of the request to create the category
+     * @return the response of the request
+     */
     private Response getResponseCategoryType(CategoryTypePayload categoryPayload) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            Response response = this.requestFactory.makeRequest().body(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(categoryPayload))
+            return this.requestFactory.makeRequest().body(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(categoryPayload))
                     .post(DataConstantQueries.PATH_CATEGORY_CREATE);
-            return response;
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             throw new RuntimeException();
         }
     }
 
+    /**
+     * this deleteCategory method send a delete request to delete a category by id
+     *
+     * @param categoryTypeId it's the category id
+     * @return
+     */
     public Response deleteCategory(String categoryTypeId) {
         return this.requestFactory.makeRequest().body("{}")
                 .delete(DataConstantQueries.PATH_DELETE_CATEGORY_TYPE + categoryTypeId);
     }
 
+    /**
+     * this getAllCategories method send a get request to obtains all categories
+     *
+     * @return the response of the request
+     */
     public Response getAllCategories() {
         return this.requestFactory.makeRequest()
                 .get(DataConstantQueries.QUERY_TO_GET_ALL_CATEGORIES);
     }
 
+    public Map<String, ?> getCurrentCategory() {
+        List<HashMap<String, ?>> listOfGroupsIdFromSpecificFirm = JsonPath.from(getAllCategories().asString()).get();
+        HashMap<String, ?> stringHashMap = listOfGroupsIdFromSpecificFirm.get(listOfGroupsIdFromSpecificFirm.size() - 1);
+        return stringHashMap;
+    }
+
+    /**
+     * this wipeOldCategories rerun the category list and delete the category who contains the automation name
+     */
     public void wipeOldCategories() {
         List<LinkedHashMap<String, ?>> listOfGroupsIdFromSpecificFirm = JsonPath.from(getAllCategories().asString()).get();
         for (LinkedHashMap<String, ?> stringHashMap : listOfGroupsIdFromSpecificFirm) {
--- src/test/resources/features/Category/CategoryAPI.feature
+++ src/test/resources/features/Category/CategoryAPI.feature
@@ -1,10 +1,12 @@
 @Regression @Category
-Feature: CategoryUI
+Feature: CategoryAPI
 
-  @CategoryUI
+  Background:
+    Given An "normalAccount" is logged in the system
+    And User wipes the old categories
+
+  @CategoryAPI
   Scenario: Create category
-    Given An "normalAccount" is logged in the system
-    And User wipes the old categories
     Given setUp "true"
     When Login page is displayed
     And User completes Email field with "normalAccount" credentials
@@ -17,41 +19,35 @@ Feature: CategoryUI
     Then The category modal is displayed
     When User enters the category information
     Then the category was created correctly
+    When User deletes the current category
+    Then the category was delete correctly
 
-  @CategoryUI
-  Scenario: Create and delete category
-    Given An "normalAccount" is logged in the system
-    And User wipes the old categories
-    Given setUp "true"
-    When Login page is displayed
-    And User completes Email field with "normalAccount" credentials
+  @CategoryAPIs
+  Scenario: Delete category
+    Given User adds a new "Root" category
+    When setUp "true"
+    Then Login page is displayed
+    When User completes Email field with "normalAccount" credentials
     And User completes Password field with "normalAccount" credentials
     And User clicks on Login button
     Then the Home page is displayed
     When User hits category option
     Then the category page is displayed
-    When User clicks Add category button
-    Then The category modal is displayed
-    When User enters the category information
-    Then the category was created correctly
     When User deletes the category "true"
     Then the category was delete UI
 
-  @CategoryUI
-  Scenario: Create and update category
-    Given An "normalAccount" is logged in the system
-    And User wipes the old categories
-    Given setUp "true"
-    When Login page is displayed
-    And User completes Email field with "normalAccount" credentials
+  @CategoryAPI
+  Scenario: Create update and delete category
+    Given User adds a new "Root" category
+    When setUp "true"
+    Then Login page is displayed
+    When User completes Email field with "normalAccount" credentials
     And User completes Password field with "normalAccount" credentials
     And User clicks on Login button
     Then the Home page is displayed
     When User hits category option
     Then the category page is displayed
-    When User clicks Add category button
-    Then The category modal is displayed
-    When User enters the category information
-    Then the category was created correctly
     When User updates the category "true"
     Then the category is correctly updated
+    When User deletes the current category
+    Then the category was delete correctly
